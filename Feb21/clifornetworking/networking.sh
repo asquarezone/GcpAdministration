41b4dd8d882e0e2a2f11893617b5eef033a43a6c
qtdevops <qtdevops@gmail.com>
2021-02-14 18:04:18 +0530

    Added delete subnets

--- Feb21/clifornetworking/networking.sh
+++ Feb21/clifornetworking/networking.sh
@@ -4,3 +4,10 @@ gcloud compute networks create 'my-vpc-from-cli' --description 'this is from cli
 gcloud compute networks subnets create 'web-useast1' --network 'my-vpc-from-cli' --range '10.10.0.0/24' --region 'us-east1'
 gcloud compute networks subnets create 'web-uswest1' --network 'my-vpc-from-cli' --range '172.16.0.0/24' --region 'us-west1'
 gcloud compute networks subnets create 'web-uscentral1' --network 'my-vpc-from-cli' --range '192.168.0.0/24' --region 'us-central1'
+
+# delete subnets
+gcloud compute networks subnets delete 'web-uscentral1' --region 'us-central1'
+gcloud compute networks subnets delete 'web-uswest1' --region 'us-west1'
+gcloud compute networks subnets delete 'web-useast1' --region 'us-east1'
+
+gcloud compute networks delete 'my-vpc-from-cli'
